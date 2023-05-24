039292a940d7c2a6941e4424037010582a55f183
developerboy1990 <mr.elahi.ehsan@gmail.com>
2023-05-25 00:16:00 +0500

    stripe

---
+++ app/Models/PaymentWithCreditCard.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PaymentWithCreditCard extends Model
+{
+    use HasFactory;
+
+    protected $table = 'payment_with_credit_cards';
+
+    protected $fillable =[
+
+        "payment_id", "customer_id", "customer_stripe_id", "charge_id"
+    ];
+
+}
